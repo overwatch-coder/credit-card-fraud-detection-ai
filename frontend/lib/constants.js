ad04c1c8b698d370b9df5b48c45f8364aadcf799
Nyamadi Atsu <95551699+overwatch-coder@users.noreply.github.com>
2025-05-01 19:15:24 +0100

    Update constants.js

--- frontend/lib/constants.js
+++ frontend/lib/constants.js
@@ -1,1 +1,2 @@
-export const API_URL = "http://localhost:5000";
+// export const API_URL = "http://localhost:5000";
+export const API_URL = "http://ec2-13-48-10-65.eu-north-1.compute.amazonaws.com/api";
